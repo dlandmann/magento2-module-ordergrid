abe7aa75b43384e2811398c9b552485940763270
Dierk Landmann <d.landmann@impericon.com>
2019-04-15 13:53:44 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,4 +1,4 @@
 <?php
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MarkShust_OrderGrid', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Dlandmann_OrderGrid', __DIR__);
